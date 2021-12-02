247a503b3729f8c286c3716b060f016975948bab
Christin Xaviour <christinxaviour@gmail.com>
2021-12-02 09:43:20 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  title: string = 'Demo App';
+  now = new Date();
+  currYear = this.now.getFullYear();
+}
